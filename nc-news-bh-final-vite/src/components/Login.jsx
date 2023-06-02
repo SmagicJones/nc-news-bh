974035b93bc76e58126db0475b1cb516562f5b6b
Smagic Jones <>
2023-06-02 10:05:54 +0100

    work in progress - login page

--- nc-news-bh-final-vite/src/components/Login.jsx
+++ nc-news-bh-final-vite/src/components/Login.jsx
@@ -1,6 +1,48 @@
-const Login = ()=>{
+import {useEffect} from 'react'
+import {fetchUsers} from '../utils/api.js';
+
+const Login = ({user, setUser})=>{
+//    console.log(user)
+
+let isValidUser = false
+
+
+
+
+
+  
+
+   function handleSubmit(e){
+    e.preventDefault();
+    console.log(isValidUser)
+    useEffect(()=>{
+        fetchUsers().then(({users})=>{
+            console.log(users)
+            users.forEach(userInDatabase => {
+                if (userInDatabase === user){
+                       isValidUser = true
+                        }
+                })
+        })
+    },[])
+   
+   }
+
+
+   
+
     return(
-        <h1>I'm a login page</h1>
+        <>
+        <form id='comment-form' onSubmit={handleSubmit}>
+        <fieldset>
+        <legend>Please Login</legend>
+        <label htmlFor="username">Enter your name: </label>
+        <input id="username" type="text" name="username" value={user} onChange={(e)=>{setUser(e.target.value)}}/>
+        <button>submit</button>
+        </fieldset>
+        </form>
+        <p>You are logged in as:  {user}</p>
+        </>
     )
 
 }
